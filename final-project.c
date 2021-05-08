97ab41120169195f6d4cf65466ccd3f763b9ef82
khaled-Ramdan <khaled1953341@feng.bu.edu.eg>
2021-05-08 17:25:12 +0200

    adding applection file and adding sort_append function

--- final-project.c
+++ final-project.c
@@ -2,18 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-struct node {
+struct Node {
 	char name[30];
 	char type[30];
 	char about[100];
 	int price;
     int id;
-    struct node *next;
+    struct Node *next;
 };
-typedef struct node node;
-struct node *head = NULL;
-struct node *current = NULL;
-
+typedef struct Node node;
+struct Node *head = NULL;
+struct Node *current = NULL;
+void sortedAppend(int id, int price,char*name,char*type,char*about);
 void main() {
    //temporary used elements
     char tmpName[30];
@@ -23,4 +23,30 @@ void main() {
     int tmpId;
     node*tmpNode;
     //reading from the file
+}
+void sortedAppend(int id, int price,char*name,char*type,char*about)
+{
+   node *newnode = (node *)malloc(sizeof(node));//making a new node in the memory
+   //assigning data
+   newnode->id = id;
+   newnode->price=price;
+   strcpy(newnode->name,name);
+   strcpy(newnode->type,type);
+   strcpy(newnode->about,about);
+   //conecting the node in a porper place in the list
+   newnode->next=NULL;
+   node*currentPtr=head,*previousPtr=NULL;
+   while (currentPtr != NULL && id >= currentPtr->id) {
+      previousPtr = currentPtr; // walk to ...
+      currentPtr = currentPtr->next; // ... next node
+   }
+   if(previousPtr==NULL)
+   {
+      newnode->next=head;
+      head=newnode;
+   }
+   else{
+      previousPtr->next=newnode;
+      newnode->next=currentPtr;
+   }
 }
